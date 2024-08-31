2d6a57d0bbcf34da683f331ae010c82b2bcae56f
iNko97 <26715474+iNko97@users.noreply.github.com>
2024-08-31 18:38:49 +0200

    Optimised model effishiency

--- model.py
+++ model.py
@@ -31,7 +31,7 @@ I = order.Items  # I: item types with their Width, Length, and Demand
 # This is essentially a matrix of all x_j
 # Each row is indexed by available widths.
 # Note how for a certain w, idx = W.index(w),then the set J^w' = J[idx, :]
-J = np.zeros((len(W), n_s_max))
+J = (len(W), n_s_max)
 
 # Subsets of I compatible with stock size j (C_j)
 # Although referring to the J matrix, it only depends on W values.
@@ -49,7 +49,7 @@ K_cj = {}
 #  dictionary with (idx) : Delta_j, that is the minimum difference between two lmin_cjk
 Delta_j = {}
 
-for idx in range(J.shape[0]):
+for idx in range(J[0]):
     for c in C_j[idx]:
         (_kmin_cj, _kmax_cj, _lmin_cjk) = order.optimised_stocksize_variables(c, W[idx])
 
@@ -59,7 +59,7 @@ for idx in range(J.shape[0]):
 # PRE-PROCESSING AND MODEL DIMENSIONS REDUCTION
 
 # Proposition 1: Generalised not just for k-1 but for the biggest k' smaller than k
-for idx in range(J.shape[0]):
+for idx in range(J[0]):
     for c in C_j[idx]:
         for k in K_cj[c, idx]:
             if k == min(K_cj[c, idx]):
@@ -69,7 +69,7 @@ for idx in range(J.shape[0]):
                 K_cj[c, idx].remove(k)
 
 # Proposition 2: Substituting I_c with |I_c| cutting pattern with lower lower bounds
-for idx in range(J.shape[0]):
+for idx in range(J[0]):
     for c in C_j[idx]:
         c_primes = sorted([1 << i for i in range(c.bit_length()) if c & (1 << i)], reverse=True)
         if len(c_primes) == 1:
@@ -86,7 +86,7 @@ for idx in range(J.shape[0]):
 
 
 # \Delta_j, the minimum length difference between two different values of lmin_cjk for a stock j
-for idx in range(J.shape[0]):
+for idx in range(J[0]):
     best_Delta = 32767
     for c_1 in C_j[idx]:
         for c_2 in C_j[idx]:
@@ -115,8 +115,8 @@ n_c_asterisk = order.best_nc
 # \alpha_{cj} 1 if the subset of item types I_c is assigned to stock size j, 0 otherwise for j \in J, c \in C
 alpha_cj = model.addVars(
     [(c, idx, n)
-     for idx in range(J.shape[0])
-     for n in range(J.shape[1])
+     for idx in range(J[0])
+     for n in range(J[1])
      for c in C_j[idx]
      ],
     vtype=GRB.BINARY,
@@ -126,8 +126,8 @@ alpha_cj = model.addVars(
 # \beta_j 1 if stock size j is selected, 0 otherwise for j \in J
 beta_j = model.addVars(
     [(idx, n)
-     for idx in range(J.shape[0])
-     for n in range(J.shape[1])
+     for idx in range(J[0])
+     for n in range(J[1])
      ],
     vtype=GRB.BINARY,
     name="beta_j"
@@ -137,8 +137,8 @@ beta_j = model.addVars(
 # 0 otherwise, for j \in J, c \in C_j, k \in K_{cj}
 gamma_cjk = model.addVars(
     [(c, idx, n, k)
-     for idx in range(J.shape[0])
-     for n in range(J.shape[1])
+     for idx in range(J[0])
+     for n in range(J[1])
      for c in C_j[idx]
      for k in K_cj[(c, idx)]
      ],
@@ -158,8 +158,8 @@ delta_w = model.addVars(
 # x_j length of stock size j, for j \in J
 x_j = model.addVars(
     [(idx, n)
-     for idx in range(J.shape[0])
-     for n in range(J.shape[1])
+     for idx in range(J[0])
+     for n in range(J[1])
      ],
     vtype=GRB.CONTINUOUS,
     lb=0,
@@ -171,8 +171,8 @@ x_j = model.addVars(
 # Note that y_{cjk} = x_j if \gamma_{cjk} = 1, otherwise 0
 y_cjk = model.addVars(
     [(c, idx, n, k)
-     for idx in range(J.shape[0])
-     for n in range(J.shape[1])
+     for idx in range(J[0])
+     for n in range(J[1])
      for c in C_j[idx]
      for k in K_cj[(c, idx)]],
     vtype=GRB.CONTINUOUS,
@@ -185,8 +185,8 @@ y_cjk = model.addVars(
 model.setObjective(
     gp.quicksum(
         W[idx] * k * y_cjk[c, idx, n, k]
-        for idx in range(J.shape[0])
-        for n in range(J.shape[1])
+        for idx in range(J[0])
+        for n in range(J[1])
         for c in C_j[idx]
         for k in K_cj[(c, idx)]
     ),
@@ -198,8 +198,8 @@ model.setObjective(
 # 3. Ensure that each item is allocated to a stock size
 model.addConstrs(
     (gp.quicksum(alpha_cj[c, idx, n] * a_ic[i][c]
-                 for idx in range(J.shape[0])
-                 for n in range(J.shape[1])
+                 for idx in range(J[0])
+                 for n in range(J[1])
                  for c in C_j[idx]) == 1
      for i in range(len(I))),
     name='link_alpha_a'
@@ -208,8 +208,8 @@ model.addConstrs(
 # 4. Ensure that a stock size is used iff at least 1 item type is assigned to it
 model.addConstrs(
     (alpha_cj[c, idx, n] <= beta_j[idx, n]
-     for idx in range(J.shape[0])
-     for n in range(J.shape[1])
+     for idx in range(J[0])
+     for n in range(J[1])
      for c in C_j[idx]),
     name="link_alpha_beta"
 )
@@ -218,8 +218,8 @@ model.addConstrs(
 model.addConstr(
     gp.quicksum(
         beta_j[idx, n]
-        for idx in range(J.shape[0])
-        for n in range(J.shape[1])
+        for idx in range(J[0])
+        for n in range(J[1])
     ) <= n_s_max,
     name="limit_stock_sizes"
 )
@@ -227,8 +227,8 @@ model.addConstr(
 # 6. Ensure that if a subset I_c is assigned to j, then a k is assigned.
 model.addConstrs(
     (gp.quicksum(gamma_cjk[c, idx, n, k] for k in K_cj[(c, idx)]) == alpha_cj[c, idx, n]
-     for idx in range(J.shape[0])
-     for n in range(J.shape[1])
+     for idx in range(J[0])
+     for n in range(J[1])
      for c in C_j[idx]),
     name="link_gamma_alpha"
 )
@@ -237,8 +237,8 @@ model.addConstrs(
 model.addConstrs(
     (gp.quicksum(lmin_cjk[(c, idx, k)] * gamma_cjk[c, idx, n, k]
                  for k in K_cj[(c, idx)]) <= x_j[idx, n]
-     for idx in range(J.shape[0])
-     for n in range(J.shape[1])
+     for idx in range(J[0])
+     for n in range(J[1])
      for c in C_j[idx]),
     name="length_bound_7"
 )
@@ -246,8 +246,8 @@ model.addConstrs(
 # 8. Ensure that the y_cjk respects its length st.
 model.addConstrs(
     (y_cjk[c, idx, n, k] <= x_j[idx, n]
-     for idx in range(J.shape[0])
-     for n in range(J.shape[1])
+     for idx in range(J[0])
+     for n in range(J[1])
      for c in C_j[idx]
      for k in K_cj[(c, idx)]),
     name="link_8_y_x"
@@ -256,8 +256,8 @@ model.addConstrs(
 # 9. Ensure that y_cjk doesn't exceed maximum length
 model.addConstrs(
     (y_cjk[c, idx, n, k] <= L_max * gamma_cjk[c, idx, n, k]
-     for idx in range(J.shape[0])
-     for n in range(J.shape[1])
+     for idx in range(J[0])
+     for n in range(J[1])
      for c in C_j[idx]
      for k in K_cj[(c, idx)]),
     name="link_9_y_x"
@@ -266,8 +266,8 @@ model.addConstrs(
 # 10. Ensure that x_j doesn't exceed maximum length if k panels are being produced.
 model.addConstrs(
     (x_j[idx, n] - y_cjk[c, idx, n, k] <= L_max * (1 - gamma_cjk[c, idx, n, k])
-     for idx in range(J.shape[0])
-     for n in range(J.shape[1])
+     for idx in range(J[0])
+     for n in range(J[1])
      for c in C_j[idx]
      for k in K_cj[(c, idx)]),
     name="link_10_y_x"
@@ -276,51 +276,51 @@ model.addConstrs(
 # 11. Ensure x_j doesn't exceed minimum length for all selected stock sizes j, otherwise 0.
 model.addConstrs(
     (L_min * beta_j[idx, n] <= x_j[idx, n]
-     for idx in range(J.shape[0])
-     for n in range(J.shape[1])),
+     for idx in range(J[0])
+     for n in range(J[1])),
     name="stock_length_min"
 )
 # 11b. Ensure x_j doesn't exceed maximum length for all selected stock sizes j, otherwise 0.
 model.addConstrs(
     (x_j[idx, n] <= L_max * beta_j[idx, n]
-     for idx in range(J.shape[0])
-     for n in range(J.shape[1])),
+     for idx in range(J[0])
+     for n in range(J[1])),
     name="stock_length_max"
 )
 
 # 12. Ensure a stock size width is used iff used at least one time.
 model.addConstrs(
-    (gp.quicksum(beta_j[idx, n] for n in range(J.shape[1])) >= delta_w[idx]
-     for idx in range(J.shape[0])),
+    (gp.quicksum(beta_j[idx, n] for n in range(J[1])) >= delta_w[idx]
+     for idx in range(J[0])),
     name="link_delta_beta"
 )
 
 model.addConstrs(
     (beta_j[idx, n] <= delta_w[idx]
-     for idx in range(J.shape[0])
-     for n in range(J.shape[1])),
+     for idx in range(J[0])
+     for n in range(J[1])),
     name="link_beta_delta"
 )
 
 # 13. Limit the number of stock widths
 model.addConstr(
-    gp.quicksum(delta_w[idx] for idx in range(J.shape[0])) <= n_w_max,
+    gp.quicksum(delta_w[idx] for idx in range(J[0])) <= n_w_max,
     name="limit_stock_widths"
 )
 
 # 21. Symmetry breaking constraint for \beta_j
 model.addConstrs(
     (beta_j[idx, n+1] <= beta_j[idx, n]
-     for idx in range(J.shape[0])
-     for n in range(J.shape[1] - 1)),
+     for idx in range(J[0])
+     for n in range(J[1] - 1)),
     name="beta_symmetry"
 )
 
 # 22. Symmetry breaking constraints for \x_j
 model.addConstrs(
     (x_j[idx, n] - x_j[idx, n+1] >= Delta_j[idx] * beta_j[idx, n+1]
-     for idx in range(J.shape[0])
-     for n in range(J.shape[1] - 1)),
+     for idx in range(J[0])
+     for n in range(J[1] - 1)),
     name="x_symmetry"
 )
 
@@ -331,8 +331,8 @@ model.optimize()
 # Extract the solution
 if model.status == GRB.OPTIMAL:
     print("Optimal solution found:")
-    for idx in range(J.shape[0]):
-        for n in range(J.shape[1]):
+    for idx in range(J[0]):
+        for n in range(J[1]):
             if beta_j[idx, n].x > 0.5:
                 print(f"Stock size {idx}, {n}: width = {W[idx]}, length = {x_j[idx, n].x}")
                 for c in C_j[idx]:
@@ -340,7 +340,7 @@ if model.status == GRB.OPTIMAL:
                         components = [i for i in range(len(I)) if c & (1 << (len(I) - 1 - i))]
                         for k in K_cj[(c, idx)]:
                             if gamma_cjk[c, idx, n, k].x > 0.5:
-                                print(f"    {k} panels, y_cjk = {y_cjk[c, idx, n, k].x}")
-                                print(f"  Items number {components} with rows {n_c_asterisk[(c, idx, k)]}:")
+                                print(f"    {k} panels of items {components}")
+                                print(f"        with respectively {n_c_asterisk[(c, idx, k)]} columns.")
 else:
     print("No optimal solution found.")
