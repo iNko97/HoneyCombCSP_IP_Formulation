72f7a6b919dbad955af7546df0dbe99c4f756f8c
Francesco Trevisan <FRANCESCO.TREVISAN@studenti.units.it>
2024-09-02 03:10:58 +0200

    Added performance loop for every scenario

--- performance.py
+++ performance.py
@@ -9,12 +9,20 @@ performanceGAP = []
 performanceTIME= []
 ordernum =[]
 result = []
-for a in ordernumbers:
-    result = model.Optimize(a,1,1)
-    ordernum.append(a)
-    performanceGAP.append(result[1])
-    performanceTIME.append(round(result[2],2))
-    performanceAREA.append(int(result[0]/1000000))
+settings = []
+for scenario in [1,2,3]:
+    for nsmax in [1,2]:
+        if scenario==3 and nsmax == 1:
+            continue
+        for a in ordernumbers:
+            stringa = "Scenario:" + str(scenario) + " NsMax:" + str(nsmax)
+            print(stringa)
+            settings.append(stringa)
+            result = model.Optimize(a,scenario,nsmax)
+            ordernum.append(a)
+            performanceGAP.append(round(result[1]*100,2))
+            performanceTIME.append(round(result[2],2))
+            performanceAREA.append(int(result[0]/1000000))
 """datapath = "Data/input_data.ods"
 datasheet = pd.ExcelFile(datapath, engine="odf")
 orders = []
@@ -42,10 +50,13 @@ fig, ax = plt.subplots()
 fig.patch.set_visible(False)
 ax.axis('off')
 ax.axis('tight')
-performance = [ordernum,performanceAREA,performanceGAP,performanceTIME]
+performance = [settings,ordernum,performanceAREA,performanceGAP,performanceTIME]
 performance = numpy.array(performance).transpose()
-df = pd.DataFrame(data=performance,columns=['ORDER','AREA','GAP','TIME'])
-ax.table(cellText=df.values, colLabels=df.columns, loc='center')
+df = pd.DataFrame(data=performance,columns=['SETTINGS','ORDER','AREA','GAP','TIME'])
+table =ax.table(cellText=df.values, colLabels=df.columns, loc='center')
+table.auto_set_font_size(False)
+table.set_fontsize(10)
+
 
 fig.tight_layout()
 plt.show()
